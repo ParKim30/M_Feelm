d39e8272cbcf3f761568027eb876e8da1fb0ee2e
Park Yejin <s2018s30@e-mirim.hs.kr>
2020-05-10 16:54:15 +0900

    firbase에서 데이터 읽어와서 나의 영화 순위 정렬

--- app/src/main/java/com/example/m_feelm/FragmentStatistic_my.java
+++ app/src/main/java/com/example/m_feelm/FragmentStatistic_my.java
@@ -1,12 +1,29 @@
 package com.example.m_feelm;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Comparator;
 
 
 /**
@@ -14,14 +31,69 @@ import android.view.ViewGroup;
  */
 public class FragmentStatistic_my extends Fragment {
 
+
     public static FragmentStatistic_my newInstance(){
         return new FragmentStatistic_my();
     }
+
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mReference;
+    private ChildEventListener mChild;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistic_my, container, false);
+        final View root = inflater.inflate(R.layout.fragment_statistic_my, container, false);
+        myRanking(root);
+
+
+        return root;
     }
+    private void myRanking(View view){
+        FirebaseUser user;
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+
+        TextView myRanking1=view.findViewById(R.id.myRanking1);
+        TextView myRanking2=view.findViewById(R.id.myRanking2);
+        TextView myRanking3=view.findViewById(R.id.myRanking3);
+
+
+        mDatabase = FirebaseDatabase.getInstance();
+        mReference=mDatabase.getReference("User_review");
+
+        ArrayList<UserReview> userReviews=new ArrayList<>();
+
+        if(user!=null) {
+            mReference.orderByChild("id").equalTo(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                 @RequiresApi(api = Build.VERSION_CODES.N)
+                 @Override
+                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                     userReviews.clear();
+                     for (DataSnapshot child : dataSnapshot.getChildren()) {
+                         UserReview review=child.getValue(UserReview.class);
+                         userReviews.add(review);
+                         Log.d("User val", child.child("title").getValue().toString());
+                         Log.d("User val", child.child("rating").getValue().toString());
+
+                     }
+                     userReviews.sort(Comparator.reverseOrder());
+                     for(int i=0;i<userReviews.size();i++){
+                         if(i<3){
+                             myRanking1.setText(userReviews.get(i).getTitle());
+                         }
+                         else break;
+                     }
+                 }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Log.d("fail","datebase 읽어오기 Error");
+                }
+            });
+        }
+    }
+
+
 }
+
---
+++ app/src/main/java/com/example/m_feelm/UserReview.java
@@ -0,0 +1,80 @@
+package com.example.m_feelm;
+
+public class UserReview implements Comparable<UserReview>{
+    private String title;
+    private float rating;
+    private String id;
+    private boolean FeedYN;
+    private String UserReview;
+    private String watch_Date;
+    private String write_Date;
+
+    public UserReview(){}
+    public UserReview(String title, float rating, String Uid, boolean feedYN, String UserReview, String watchDate, String writeDate){
+        this.title=title;
+        this.rating=rating;
+        this.id=Uid;
+        this.FeedYN=feedYN;
+        this.UserReview=UserReview;
+        this.watch_Date=watchDate;
+        this.write_Date=writeDate;
+    }
+
+    public void setFeedYN(boolean feedYN) {
+        FeedYN = feedYN;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setUserReview(String userReview) {
+        UserReview = userReview;
+    }
+
+    public void setWatch_Date(String watch_Date) {
+        this.watch_Date = watch_Date;
+    }
+
+    public void setWrite_Date(String write_Date) {
+        this.write_Date = write_Date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public String getUserReview() {
+        return UserReview;
+    }
+
+    public String getWatch_Date() {
+        return watch_Date;
+    }
+
+    public String getWrite_Date() {
+        return write_Date;
+    }
+
+    @Override
+    public int compareTo(UserReview review) {
+        float targetAge = review.getRating();
+        return Float.compare(rating, targetAge);
+    }
+}
